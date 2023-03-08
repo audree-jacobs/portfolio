6bfb076c4c452bb70fcced1a92101e579a0c0c95
Audrey Jacobs <71578039+audree-jacobs@users.noreply.github.com>
2023-03-08 11:05:02 -0700

    Add files via upload

---
+++ Templar Files/script.js
@@ -0,0 +1,11 @@
+// open in40s site in a new tab when clicked on logo
+function in40s() {
+    window.open('https://www.in40s.us/', '_blank');
+}
+
+
+// rerun the gif 
+
+function gif() {
+    document.querySelector(".logo_gif").src = "images/logo gif.gif"; 
+}
